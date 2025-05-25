674c38327d7e7b0150e6c8501a34696726d9175a
vipiBitbucket <vipivipisanan@gmail.com>
2025-05-25 17:01:46 +0530

    persistent store added

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-export default configureStore({
-  reducer: {
-    auth: authReducer,
-    // add other slices here
-  },
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["auth"], // only persist the auth slice
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  // add other slices here
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+export default store;

d620a47390f03e92433fef6749f12cb247c68e01
vipiBitbucket <vipivipisanan@gmail.com>
2025-05-27 14:00:25 +0530

    code clean

--- src/forms/ProfileForm.js
+++ src/forms/ProfileForm.js
@@ -3,7 +3,6 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
-// Validation schema matching your ProfileRequestDto constraints
 const schema = yup.object().shape({
   displayName: yup
     .string()
